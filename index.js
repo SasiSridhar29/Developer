4404593b7e4b2b09e1116fbb0b6c0ed10a1f2809
Sridharsasi <49144065+Sridharsasi@users.noreply.github.com>
2023-07-11 22:01:14 -0400

    deployedgit status

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app=express();
+
+
+
+
+
+
+
+
+
+
+const PORT = process.env.PORT || 3000;
+app.listen (PORT, ()=> console.log(`Server Started on port ${PORT}!`))
+
+
